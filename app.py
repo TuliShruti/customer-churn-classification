2c2b6d84a6352939c8df201129cc4d6f106d4931
Shruti Guha <153054268+TuliShruti@users.noreply.github.com>
2026-03-30 10:18:33 +0530

    Fix Streamlit churn prediction flow duplication bug

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-import tensorflow as tf
 import pickle
+
+import pandas as pd
+import streamlit as st
+import tensorflow as tf
 
 # Load model and preprocessing objects
 model = tf.keras.models.load_model('model.h5')
@@ -41,7 +40,7 @@ if st.button('Predict'):
         'NumOfProducts': num_of_products,
         'HasCrCard': has_cr_card,
         'IsActiveMember': is_active_member,
-        'EstimatedSalary': estimated_salary
+        'EstimatedSalary': estimated_salary,
     }
     input_df = pd.DataFrame([input_dict])
 
@@ -50,14 +49,26 @@ if st.button('Predict'):
 
     # One-hot encode Geography
     geo_encoded = onehot_encoder_geo.transform(input_df[['Geography']]).toarray()
-    geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+    geo_encoded_df = pd.DataFrame(
+        geo_encoded,
+        columns=onehot_encoder_geo.get_feature_names_out(['Geography']),
+    )
     input_df = pd.concat([input_df.drop('Geography', axis=1), geo_encoded_df], axis=1)
 
     # Reorder columns to match training
     expected_cols = [
-        'CreditScore', 'Gender', 'Age', 'Tenure', 'Balance', 'NumOfProducts',
-        'HasCrCard', 'IsActiveMember', 'EstimatedSalary',
-        'Geography_France', 'Geography_Germany', 'Geography_Spain'
+        'CreditScore',
+        'Gender',
+        'Age',
+        'Tenure',
+        'Balance',
+        'NumOfProducts',
+        'HasCrCard',
+        'IsActiveMember',
+        'EstimatedSalary',
+        'Geography_France',
+        'Geography_Germany',
+        'Geography_Spain',
     ]
     input_df = input_df.reindex(columns=expected_cols, fill_value=0)
 
@@ -73,42 +84,3 @@ if st.button('Predict'):
         st.error('The customer is likely to churn.')
     else:
         st.success('The customer is not likely to churn.')
-tenure = st.slider('Tenure', 0, 10)
-num_of_products = st.slider('Number of Products', 1, 4)
-has_cr_card = st.selectbox('Has Credit Card', [0, 1])
-is_active_member = st.selectbox('Is Active Member', [0, 1])
-
-# Prepare the input data
-input_data = pd.DataFrame({
-    'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
-    'Age': [age],
-    'Tenure': [tenure],
-    'Balance': [balance],
-    'NumOfProducts': [num_of_products],
-    'HasCrCard': [has_cr_card],
-    'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
-})
-
-# One-hot encode 'Geography'
-geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
-
-# Combine one-hot encoded columns with input data
-input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
-
-# Scale the input data
-input_data_scaled = scaler.transform(input_data)
-
-
-# Predict churn
-prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
-
-st.write(f'Churn Probability: {prediction_proba:.2f}')
-
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.')
